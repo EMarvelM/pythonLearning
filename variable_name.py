4a3e1423b052ee4ef5dabd7d26f54cc45333ef44
EMarvelM <marvelousegbe19@gmail.com>
2023-09-24 00:19:10 +0100

    assigning multiple variable respectively

--- variable_name.py
+++ variable_name.py
@@ -7,3 +7,13 @@ myVariableName = "Testing"
 MyVariableName = "Testing"
 #snake case
 my_variable_name = "Testing"
+
+
+"""
+Assigning multi variables
+"""
+x, y, z = "First", "Second", "Last"
+#be sure number of variables match number of values to assign
+print(x)
+print(y)
+print(z)
