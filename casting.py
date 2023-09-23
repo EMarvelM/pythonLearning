442bae7545615dd3e3b50634a03474695d4f4437
EMarvelM <marvelousegbe19@gmail.com>
2023-09-24 00:06:41 +0100

    getting the data types

--- casting.py
+++ casting.py
@@ -1,3 +1,11 @@
 x = str(4) #4 is a string in x as "4"
 z = float("5") #5 is a float in z as 5.0
 y = int(8) #8 is an integer in y as 8
+
+"""
+The below will be used ti get the types of variables
+"""
+
+print(type(x))
+print(type(z))
+print(type(y))
