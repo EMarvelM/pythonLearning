4a72408dcb621e9b40ce30b9bb0fc387a71660ab
EMarvelM <marvelousegbe19@gmail.com>
2023-09-24 00:04:50 +0100

    simply explain

--- casting.py
+++ casting.py
@@ -1,3 +1,3 @@
-x = str(4) #4 is a string in x
-z = float(5) #5 is a float in z
-y = int(8) #8 is an integer in y
+x = str(4) #4 is a string in x as "4"
+z = float("5") #5 is a float in z as 5.0
+y = int(8) #8 is an integer in y as 8
