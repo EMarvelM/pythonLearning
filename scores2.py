db24ae15145b92cc59b8135d806b10ddd7e3c533
EMarvelM <marvelousegbe19@gmail.com>
2023-09-25 15:59:25 +0100

    this is another way to append to a list

--- scores2.py
+++ scores2.py
@@ -2,6 +2,10 @@ scores = []#created a empty list
 
 for i in range(3):
     score = int(input("score: "))
-    scores.append(score)
+    #scores.append(score)
+    
+    score += [scores]
+    #this will append
+
     average = sum(scores) / len(scores)
     print(f"average == {average:.1f}")
