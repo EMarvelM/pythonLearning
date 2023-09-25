e7dd10375239fa494cf2a086077bb868fd937f5d
EMarvelM <marvelousegbe19@gmail.com>
2023-09-25 09:08:06 +0100

    using range to avoid large list in loops

--- loops.py
+++ loops.py
@@ -4,5 +4,9 @@ while i < 3:
     i += 1#i++ is not allowed in python
 
 
-for i in [0, 1, 2]:
+for i in [0, 1, 2]:#to avoid typing long list of interation we'd use range
     print("Hello, World")
+
+for i in range(30):
+    print(f"very easy{i}")
+    i += 1
