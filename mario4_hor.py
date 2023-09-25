49b8ea964f2aa2915875c693e2cd3217d590d0bf
EMarvelM <marvelousegbe19@gmail.com>
2023-09-25 13:27:53 +0100

    rectangular pattern

--- mario4_hor.py
+++ mario4_hor.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
+"""
+TRIANGLE
+"""
 j = 1
 for i in range(4):
     k = 0
     for k in range(j):
-        print("#", end="")
+        print("*", end="")
     j += 1
 
     print()
 
 
-print("?" * 4)
+#print("?" * 4)
+
+
+"""
+RECTANGLE
+"""
+x = 3
+for i in range(x):
+    print("#" * x)
+
+#OR
+
+for i in range(x):
+    for j in range(x):
+        print("&", end="")
+    print()
