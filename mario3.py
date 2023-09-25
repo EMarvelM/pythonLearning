eb106a925fad83d91edd236dd478529f491c747c
EMarvelM <marvelousegbe19@gmail.com>
2023-09-25 11:40:42 +0100

    introducing return

---
+++ mario3.py
@@ -0,0 +1,15 @@
+def main():
+    height = get_height()
+    for i in range(height):
+        print("#")
+
+
+def get_height():
+    while True:
+        n = int(input("height: "))
+        if (n > 0):
+            break
+
+    return n
+
+main()
