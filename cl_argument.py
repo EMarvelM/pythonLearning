3bee817c79b4c9724bf0663a9442977b50d32291
EMarvelM <marvelousegbe19@gmail.com>
2023-09-25 22:50:28 +0100

    make code more simplier by using the slicing method [1:]

--- cl_argument.py
+++ cl_argument.py
@@ -6,6 +6,21 @@ if len(argv) > 1:
         if i == 0:
             print("Hello ", end="")
         print(f"{argv[i + 1]}", end=" ")#added 1 to start interating from 1 not 0
+        if i == (len(argv) - 2):
+            print()
+
+else:
+    print("Hello World")
+
+"""
+How about even more simpler using slicing
+"""
+if len(argv) > 1:
+    for argve in argv[1:]:
+        if argv[1] == argve:
+            print("Hello ", end="")
+        print(f"{argve}", end=" ")
+    print()
 
 else:
     print("Hello World")
