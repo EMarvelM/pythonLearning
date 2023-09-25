e8442dea656e639489dc16c6c8aa027a4dac7a65
EMarvelM <marvelousegbe19@gmail.com>
2023-09-25 16:00:24 +0100

    line: 7: type error with variables. thids is another way to append to a list

--- scores2.py
+++ scores2.py
@@ -3,8 +3,8 @@ scores = []#created a empty list
 for i in range(3):
     score = int(input("score: "))
     #scores.append(score)
-    
-    score += [scores]
+
+    scores += [score]
     #this will append
 
     average = sum(scores) / len(scores)
