b8cd34d3b6bf4a84a0a1c848f0b0bf07625f552b
EMarvelM <marvelousegbe19@gmail.com>
2023-09-25 16:13:53 +0100

    As simple as ABC: convert string to u upper than iterating through letters

--- uppercase.py
+++ uppercase.py
@@ -1,7 +1,17 @@
-#convert characters to upper case
+"""
+convert characters to upper case
+"""
 Before = input("Before: ")
 print("After: ", end="")
 
+#iterate through the users input
+#this appears long not when i can uppercase a string
 for c in Before:
     print(c.upper(), end="")
 print()
+
+"""
+Alternatively
+"""
+#As simple as ABC
+print(f"After: {Before.upper()}")
