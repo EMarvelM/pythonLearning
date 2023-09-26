364863ab25930729abb7678f5e1494a6ca6251a9
EMarvelM <marvelousegbe19@gmail.com>
2023-09-26 15:02:43 +0100

    capitalize the first letter

--- cl_argument.py
+++ cl_argument.py
@@ -5,7 +5,7 @@ if len(argv) > 1:
     for i in range(len(argv) - 1):#minus 1 to avoid counting the argv[0]
         if i == 0:
             print("Hello ", end="")
-        print(f"{argv[i + 1]}", end=" ")#added 1 to start interating from 1 not 0
+        print(f"{argv[i + 1].capitalize()}", end=" ")#added 1 to start interating from 1 not 0
         if i == (len(argv) - 2):
             print()
 
@@ -19,7 +19,7 @@ if len(argv) > 1:
     for argve in argv[1:]:
         if argv[1] == argve:
             print("Hello ", end="")
-        print(f"{argve}", end=" ")
+        print(f"{argve.capitalize()}", end=" ")
     print()
 
 else:
