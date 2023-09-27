5f48bb4b86bb5b8c064a1f93fa1a0b8d7b404664
EMarvelM <marvelousegbe19@gmail.com>
2023-09-27 21:46:03 +0100

    implementing the menu

--- phaseII/to_do_list.py
+++ phaseII/to_do_list.py
@@ -2,8 +2,13 @@
 
 from sys import exit
 
-lists = dict()
+lists = {}
 priority = []
+start = {
+    "1": "add",
+    "2": "remove",
+    "3": "preview"
+}
 yes = ["yes", "y"]
 no = ["no", "n"]
 prior = ["High", "Low", "Medium"]
@@ -13,8 +18,18 @@ def main():
     if chosen in yes:
         i = 0
         while True:
-            new_title= input("Title: ")
-            task = input("Description: ")
+            while True:
+                new_title = input("Title: ")
+                if new_title in start.values():
+                    next(new_title, lists)
+                if len(new_title) < 30:
+                    break
+                print("Title cannot be more than 30 characters")
+            while True:
+                task = input("Description: ")
+                if len(task) > 5:
+                    break
+                print("Description too short\nTry again!")
             while True:
                 prio = input("Priority: ")
                 prio = prio.capitalize()
@@ -53,17 +68,34 @@ def welcome():
         print()
         return choose
     else:
-"""
         i = 1
         for title in lists:
             print(f"{i}. {title.capitalize()}:", end=" ")
             print(lists[title])
             i += 1
         return None
-"""
-        print(" what would you like to do? ")
+
+def next(check, lists):
+    if check == "add":
+        main()
+    elif check == "remove":
+        i = 1
+        print("S/No" + " " * 3 + "TITLE" + " " * 35 + "DESCRIPTION")
+        for new in lists:
+            print(i, end=" " * 6)
+            print(f"{new}", end=" " * (35 - len(new)))
+            print(lists[new])
+            i += 1
+
+        j = 0
+        remover = int(input("what task would you wanna remove? "))
+        for lis in lists:
+            j += 1
+            if j == remover:
+                lists[lis] = []#TODO: DELETE THE LIST
 
 
-
+#def remove():
+#def preview():
 
 main()
