e02e7fd47eb20abaf6fcb50421385cb8065a8d41
EMarvelM <marvelousegbe19@gmail.com>
2023-09-28 21:03:03 +0100

    types of int

---
+++ primitive_data.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Primitive data type
+"""
+
+#types of int
+var_1 = 0o123#octa prefix
+var_2 = 0X123#hexa-decimal prefix
+var_3 = 123
+
+print(var_1)
+print(var_2)
+print(var_3)
+
