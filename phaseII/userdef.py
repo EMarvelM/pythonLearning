a1812a298411afe44d8c1d44b2bbc6d5dfefcb13
EMarvelM <marvelousegbe19@gmail.com>
2023-09-29 14:37:15 +0100

    and in the end

--- phaseII/userdef.py
+++ phaseII/userdef.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
 
+
+
 def leni(ch):
     if ch == 0:
         return 0
+
+    ls = []
     while True:
         #n = ch / 10
 
         if ifint(ch) == 0:
+            n = ch % 10
+            ch = int(ch / 10)
+            if (ch) >= 0:
+                ls.append(ch)
+                #print(ls)
+                #print("an int")
+                if ch == 0:
+                    # print(len(ls))
+                    return len(ls)
+            elif ch == 0:
+                break
+        else:
+            print("No longer int")
+            break
 
 def ifint(x):
     y = 0
