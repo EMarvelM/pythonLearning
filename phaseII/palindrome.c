979d8022bc335205256c3151516b1e457c901ba7
EMarvelM <marvelousegbe19@gmail.com>
2023-09-30 00:04:27 +0100

    opps an infinite loop

--- phaseII/palindrome.c
+++ phaseII/palindrome.c
@@ -2,4 +2,16 @@ bool isPalindrome(int x){
     int y, copy;
     unsigned int z = 0;
 
+
+
+    while (1)
+    {
+        y = x % 10;
+        x /= 10;
+
+        z = z * 10;
+        z += y;
+
+
+    }
 }
