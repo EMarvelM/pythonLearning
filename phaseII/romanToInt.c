0e0a669c97d53ac7461d008b263addb64847ff54
EMarvelM <marvelousegbe19@gmail.com>
2023-10-02 06:51:32 +0100

    to calculate for each roman: issues

--- phaseII/romanToInt.c
+++ phaseII/romanToInt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-int romanToInt(char * s);
+unsigned int romanToInt(char * s);
 
 /**
  * main - Entry point.
@@ -9,14 +9,20 @@ int romanToInt(char * s);
 */
 int main(void)
 {
-    romanToInt("III");
+    unsigned int total;
+
+    total = romanToInt("MCMXCIV");
+
+    printf("%d\n", total);
 
     return (0);
 }
 
-int romanToInt(char * s){
+unsigned int romanToInt(char * s){
     /*check the values*/
-    int i = 0, j, valid = -1;
+    int i = 0, j, valid = 2;
+    /*int a_I, a_V, a_X, a_L, a_C, a_D, a_M;*/
+    unsigned int total = 0;
 
     char roman[] = "IVXLCDM";
 
@@ -26,16 +32,65 @@ int romanToInt(char * s){
         {
             if (s[i] == roman[j])
             {
-                valid = 0;
-                /*printf("%c  ", s[i]);
-                printf("%c\n", roman[j]);*/
+                /*test*/
+                printf("%c  %c\n", s[i], roman[j]);
+
+                valid += 1;
+                switch (s[i]){
+                    case 'M':
+                    {
+                        total += 1000;
+                    }
+                    break;
+
+                    case 'C':
+                    if (s[i + 1] == 'M')
+                    {
+                        total += 900;
+                        i += 2;
+                        valid++;
+                    }
+                    else
+                        total += 100;
+                    break;
+
+                    case 'X':
+                    if (s[i + 1] == 'C')
+                    {
+                        total += 90;
+                        i += 2;
+                        valid++;
+                    }
+                    else
+                        total += 10;
+                    break;
+
+                    case 'I':
+                    if (s[i + 1] == 'X')
+                    {
+                        total += 9;
+                        i += 2;
+                        valid++;
+                    }
+                    else if (s[i + 1] == 'V')
+                    {
+                        total += 4;
+                        i += 2;
+                        valid++;
+                    }
+                    else
+                        total += 1;
+                    break;
+
+                }
+
             }
         }
-        if (valid == -1)
+        if (valid < 1)
         {
-            exit(1);
+            exit(-1);
         }
-        valid = -1;
+        valid--;
         i++;
     }
     if (i > 15)
@@ -43,5 +98,6 @@ int romanToInt(char * s){
         exit(1);
     }
 
-    return (i);
+    return (total);
 }
+
