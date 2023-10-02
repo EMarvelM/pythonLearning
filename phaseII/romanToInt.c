184f95d13482b68e15c7605a108b9cd1efccbb46
EMarvelM <marvelousegbe19@gmail.com>
2023-10-02 01:38:54 +0100

    never forget to prototype

--- phaseII/romanToInt.c
+++ phaseII/romanToInt.c
@@ -1,8 +1,23 @@
+int romanToInt(char * s);
+
+/**
+ * main - Entry point.
+ *
+ * Return: 0 always on sucess
+*/
 int main(void)
 {
-    romanToInt(III)
+    romanToInt(III);
+
+    return (0);
 }
 
 int romanToInt(char * s){
     /*check the values*/
+    int i = 0;
+
+    while (s[i])
+    {
+        i++;
+    }
 }
