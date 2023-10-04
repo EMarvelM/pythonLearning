f479fdbd2785c88898c054a2676b498cdf58e8d0
EMarvelM <marvelousegbe19@gmail.com>
2023-10-04 16:29:03 +0100

    function that checks for lowercase character

---
+++ p_l_d/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def islower(c):
+    # while True:
+    if ord(c) in range(97, 123):
+        print(True)
+        return True
+    else:
+        print(False)
+        return False
