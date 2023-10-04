1adfd86a83bd3c90e81cede50462d418b3df1352
EMarvelM <marvelousegbe19@gmail.com>
2023-10-04 10:51:03 +0100

    ignoring the q and e

--- p_l_d/3-print_alphabt.py
+++ p_l_d/3-print_alphabt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 x: int = ord("a")
 while True:
-    print(chr(x), end = "")
+    if x is not ord("q") and x is not ord("e"):
+        print(chr(x), end = "")
     x += 1
     #break at 'z'
     if x >= 123:
