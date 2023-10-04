20b48014d9b35639bfd58a7c7d2495a5861822ca
EMarvelM <marvelousegbe19@gmail.com>
2023-10-04 17:02:19 +0100

    keeping to requirement

--- p_l_d/8-uppercase.py
+++ p_l_d/8-uppercase.py
@@ -2,9 +2,8 @@
 
 def uppercase(str):
     for c in str:
-        if ord(c) == ord(" "):
-            print(" ", end="")
-        elif ord(c) in range(65, 91) or ord(c) in range(48, 58):
+
+        if ord(c) in range(65, 91) or ord(c) in range(48, 58) or ord(c) == 32:
             print(chr(ord(c)), end="")
         elif ord(c) in range(97, 123):
             print(chr(ord(c) - (97 - 65)), end="")
