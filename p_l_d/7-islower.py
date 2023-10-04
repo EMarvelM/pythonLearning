dcec55b789bb2a57726aa234d4c7dba30768de50
EMarvelM <marvelousegbe19@gmail.com>
2023-10-04 16:30:40 +0100

    no need to print true/false

--- p_l_d/7-islower.py
+++ p_l_d/7-islower.py
@@ -2,8 +2,13 @@
 def islower(c):
     # while True:
     if ord(c) in range(97, 123):
-        print(True)
         return True
     else:
-        print(False)
         return False
+
+#                       """ TEST """                        #
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
