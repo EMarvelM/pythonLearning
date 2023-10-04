e8d7cbb5734a84875f89c0685d3d69b25b1eaad8
EMarvelM <marvelousegbe19@gmail.com>
2023-10-04 17:00:05 +0100

    requirement:can only use no more than 2 print functions with string format

--- p_l_d/8-uppercase.py
+++ p_l_d/8-uppercase.py
@@ -4,9 +4,7 @@ def uppercase(str):
     for c in str:
         if ord(c) == ord(" "):
             print(" ", end="")
-        elif ord(c) in range(65, 91):
-            print(chr(ord(c)), end="")
-        elif ord(c) in range(48, 58):
+        elif ord(c) in range(65, 91) and ord(c) in range(48, 58):
             print(chr(ord(c)), end="")
         elif ord(c) in range(97, 123):
             print(chr(ord(c) - (97 - 65)), end="")
