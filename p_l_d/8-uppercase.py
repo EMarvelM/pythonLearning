c506893bb6d91857854e73a08590c3c627ec641d
EMarvelM <marvelousegbe19@gmail.com>
2023-10-04 16:58:09 +0100

    print for lower to upper

--- p_l_d/8-uppercase.py
+++ p_l_d/8-uppercase.py
@@ -2,7 +2,10 @@
 
 def uppercase(str):
     for c in str:
-        if ord(c) in range(97, 123):
-            print(ord(c) - (97 - 65))
 
-uppercase("ad")
+        if ord(c) in range(97, 123):
+            print(chr(ord(c) - (97 - 65)), end="")
+    print()
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
