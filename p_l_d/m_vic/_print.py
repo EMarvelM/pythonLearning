51d42ac399871068d36bd7f28150a5bf87f8c0a0
EMarvelM <marvelousegbe19@gmail.com>
2023-10-17 13:14:12 +0100

    practicing

--- p_l_d/m_vic/_print.py
+++ p_l_d/m_vic/_print.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+from pract import _add, a, _print
+
+_print()
+_add()
+print(a)
--- p_l_d/m_vic/pract.py
+++ p_l_d/m_vic/pract.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+a = 3
+def _print():
+    print("Hello world")
+
+def _add():
+    global a
+    a = 3
+    b = 5
+    print(a + b)
+
+# if we're runing from the terminal
+if __name__ == "main":
+    _print()
+    _add()
