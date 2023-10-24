d53855fa04b2438e9881f3d5dfbecc301c4fd332
EMarvelM <marvelousegbe19@gmail.com>
2023-10-24 15:44:06 +0100

    create an evn for cowsay

--- phaseIII/say.py
+++ phaseIII/say.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/home/vagrant/pythonLearning/phaseIII/bin/python3
+import cowsay
+import sys
+
+if len(sys.argv) == 2:
+    cowsay.trex("Hello, " + sys.argv[1])
+else:
+    cowsay.cow("Hello, World!")
