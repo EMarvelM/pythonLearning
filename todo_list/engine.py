f7067de4b78cd99bc462b2ac44cd084428cb4764
EMarvelM <marvelousegbe19@gmail.com>
2024-02-04 11:14:27 +0100

    fix: now saves details to json

--- todo_list/engine.py
+++ todo_list/engine.py
@@ -7,44 +7,57 @@ import os
 class TodoObject(object):
     _file = "file.json"
 
-    def __init__(self):
-        off_set = None
-        title = None
-        description = None
-        priority = None
+    def __init__(self, title):
+        self.off_set = None
+        self.title = title
+        self.description = None
+        self.priority = None
+
+        # holds the dictionary data
+        self.__object = []
 
         self.reload()
         self.update()
 
     def to_dict(self):
-        return self.__dict__
+        return {
+            "off_set": self.off_set,
+            "title": self.title,
+            "description": self.description,
+            "priority": self.priority,
+        }
 
     def reload(self):
         if os.path.isfile(self._file):
             with open(self._file, "r") as file:
-                # content = file.read()
-                self._object = json.load(file)
-        else:
-            self._object = None
+                data = json.load(file)
+                if data["todo"]:
+                    self.__object = data["todo"]
 
 
     def update(self):
         i = 1
-        if self._object:
-            while True:
-                for arrange in self._object["todo"]:
-                    if arrange["off_set"] == i:
-                        pass
-                    else:
-                        self.off_set = i
-                        data = self.to_dict()
-                        self._object.append(data)
-                    i += 1
+        if self.__object:
+            for arrange in self.__object:
+                if arrange["off_set"] == i:
+                    # print(f'seen {arrange["title"]}')
+                    pass
+                else:
+                    break
+                i += 1
+            self.off_set = i
+            data = self.to_dict()
+            self.__object.append(data)
+
+        else:
+            self.off_set = i
+            data = self.to_dict()
+            self.__object.append(data)
+
 
 
     def save(self):
-        self.reload()
-        # self._object["todo"] # todo: edit and dump back
+        # todo: updating the value before saving
         with open(self._file, "w") as file:
-            data = {"todo": self._object}
+            data = {"todo": self.__object}
             json.dump(data, file, indent=2)
